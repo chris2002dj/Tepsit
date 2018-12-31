e6c7c2ff0475cc4336ab6a31db5c1e5ab1add43f
Chris Deboli <christopher2002buffon@gmail.com>
2018-12-31 18:35:21 +0100

    Ho spiegato la riga 12, 15

--- Tepsit/Program.cs
+++ Tepsit/Program.cs
@@ -13,10 +13,10 @@ namespace Tepsit
             int a = int.MaxValue;
             Console.WriteLine(a); //Rappresenta il numero massimo supportato dall'int 
             Console.WriteLine(a + 1); //Rappresenta il numero minimo supportato aggiungendo +1
-            int b = -128;
+            int b = -128; //HO dato come valore -128 nell'int b
             int c = -b;
             Console.WriteLine(Convert.ToString(b, 2));
-            Console.WriteLine(Convert.ToString(c, 2));
+            Console.WriteLine(Convert.ToString(c, 2)); //Il valore c lo divido in base 2
             int value = 31;
             int somma = value;
             value = ~value + 1; //NOT
